1004c9806e8ba3bf44d38b67f5b416383e5aa411
maxgerashenko <mgerashchenko@gmail.com>
2023-03-04 11:35:56 -0500

    1 file modified

--- app/numberAndCode.js
+++ app/numberAndCode.js
@@ -1,39 +1,39 @@
-import React, { Component } from "react";
-import Link from "@material-ui/core/Link";
+import React, { Component } from 'react';
+import Link from '@material-ui/core/Link';
 
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
-import ButtonBase from "@material-ui/core/ButtonBase";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import ButtonBase from '@material-ui/core/ButtonBase';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   paper: {
     padding: theme.spacing(2),
-    margin: "auto",
-    maxWidth: 500
+    margin: 'auto',
+    maxWidth: 500,
   },
   image: {
     width: 128,
-    height: 128
+    height: 128,
   },
   img: {
-    margin: "auto",
-    display: "block",
-    maxWidth: "100%",
-    maxHeight: "100%",
-  }
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+  },
 }));
 
-export default ({ code = '', title='' }) => {
+export default ({ code = '', title = '' }) => {
   const classes = useStyles();
   const displayTitle = title.charAt(0).toUpperCase() + title.slice(1);
 
-  const valueTemplate = value => (
+  const valueTemplate = (value) => (
     <Grid item xs={4} sm={4} md={4}>
       <Paper className={classes.paper}>{value}</Paper>
     </Grid>
@@ -41,17 +41,14 @@ export default ({ code = '', title='' }) => {
 
   console.log('code', code);
 
-  if(code.length) {
-    const codes = String(code).split(",").map(valueTemplate);
-  }
-
+  const codes = String(code).split(',').map(valueTemplate);
 
   return (
     <div className={classes.root}>
       <Paper className={classes.paper}>
         <Grid item xs={12}>
           <Typography gutterBottom variant="subtitle1">
-            {displayTitle }
+            {displayTitle}
           </Typography>
         </Grid>
         <Grid item xs container spacing={2}>
